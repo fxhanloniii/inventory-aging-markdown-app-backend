e9a3b2d98a14c49174a0b91d3372625146ce987a
fxhanloniii <hanlonfrankiex@gmail.com>
2023-08-08 13:08:08 -0700

    setting up app

--- server.js
+++ server.js
@@ -2,3 +2,25 @@ require('@shopify/shopify-api/adapters/node');
 const express = require('express');
 const { shopifyApi, LATEST_API_VERSION } = require('@shopify/shopify-api');
 const dotenv = require('dotenv')
+
+dotenv.config();
+
+const shopify = shopifyApi({
+    apiKey: process.env.API_KEY,
+    apiSecretKey: process.env.API_SECRET_KEY,
+    scopes: [process.env.SCOPES],
+    hostName: process.env.HOST_NAME,
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
